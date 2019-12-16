e93cbbd76815cb51fa75ead9015ea36797d48c1e
wealthintheday <lyclyc17@gmail.com>
2019-12-16 22:26:32 +0800

    test: add unit test

--- src/lib/util.ts
+++ src/lib/util.ts
@@ -1,9 +1,20 @@
+import { isNumber } from "util";
+
 /** 从 数组中随机选取 n 个元素 */
 export function random(arr: any[], n: number) {
   const result = [];
   let m = arr.length;
+  if (n > arr.length) {
+    return arr;
+  }
+
+  if (!isNumber(n) || n < 0) {
+    return null;
+  }
+
   let t;
   let i;
+
   while (m && result.length < n) {
     i = Math.floor(Math.random() * m--);
     t = arr[m];
